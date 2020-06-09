88d32441be6dfac0a798baa034b3c82b1e0e6210
Rahul Shakya <rahulshakya123rs@gmail.com>
2020-06-09 09:09:33 +0545

    Templeting of admin panel finished

---
+++ resources/views/adminpartial/footer.blade.php
@@ -0,0 +1,44 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2020 <a href="http://rahulshakya.xyz" target="_blank">Developed by Rahul Shakya</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 1.0.0
+    </div>
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="{{asset('adminexternal/plugins/jquery/jquery.min.js')}}"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="{{asset('adminexternal/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+    $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('adminexternal/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- Sparkline -->
+<script src="{{asset('adminexternal/plugins/sparklines/sparkline.js')}}"></script>
+<!-- jQuery Knob Chart -->
+<script src="{{asset('adminexternal/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
+<!-- daterangepicker -->
+<script src="{{asset('adminexternal/plugins/moment/moment.min.js')}}"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="{{asset('adminexternal/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+<!-- overlayScrollbars -->
+<script src="{{asset('adminexternal/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('adminexternal/js/adminlte.js')}}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{asset('adminexternal/js/pages/dashboard.js')}}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{asset('adminexternal/js/demo.js')}}"></script>
+</body>
+</html>
